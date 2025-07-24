d3998cd4c488c01756d7f0c78cc7fa152f523280
Niklas Spielbauer <spielbauer@fzi.de>
2025-07-24 09:44:12 +0200

    change colorscheme
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,13 @@
 return {
-  -- Theme inspired by Atom
-  'navarasu/onedark.nvim',
-  priority = 1000,
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.cmd.colorscheme 'onedark'
+    require('github-theme').setup({
+      -- ...
+    })
+
+    vim.cmd('colorscheme github_dark')
   end,
 }
